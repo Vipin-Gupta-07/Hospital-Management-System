8837bb7d9a7b946cc44f8eafec60199d98c9b0cd
Vipin-Gupta-07 <vipingupta930548@gmail.com>
2026-05-24 18:37:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import random
 import string
 import sys
 
-#---------------------------------------------------------------------
+#-------------------------------------------------------------------
+
 
 class HospitalManagementSystem:
     def __init__(self):
